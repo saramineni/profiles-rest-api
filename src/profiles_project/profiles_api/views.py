20cd309801c880b181a75fc789fa7ce07f00d5a5
Sai Krishna Ramineni -X (saramine - TATA CONSULTANCY SERVICES LIMITED at Cisco) <saramine@cisco.com>
2017-09-08 00:18:04 -0700

    added a post method to apiview

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 # Create your views here.
 
 class HelloApiView(APIView):
     """testing api view"""
-
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format = None):
 
@@ -16,3 +18,14 @@ class HelloApiView(APIView):
         ]
 
         return Response({"message":"Hello there","an_apiview":an_apiview})
+
+    def post(self, request):
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello{0}'.format(name)
+            return Response({'messsage':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
