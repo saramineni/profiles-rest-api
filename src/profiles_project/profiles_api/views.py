b12bffe13dd23456d5ad2e9225461a7aad3466d9
Sai Krishna Ramineni -X (saramine - TATA CONSULTANCY SERVICES LIMITED at Cisco) <saramine@cisco.com>
2017-09-07 22:57:46 -0700

    added a get method in apiview

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+    """testing api view"""
+
+
+    def get(self, request, format = None):
+
+        an_apiview=[
+            'this is for logical processing',
+            'this uses http methods',
+            'this gives more freedom to communicate with other apis'
+        ]
+
+        return Response({"message":"Hello there","an_apiview":an_apiview})
